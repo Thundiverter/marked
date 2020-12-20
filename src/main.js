f08b580cf7fe1eb8a89a7141b054a33ca48d51dc
Ivan Stickmanov <stickman9000@mail.ru>
2020-12-20 19:50:26 +0300

    Getting data from URL doesn't work anymore

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ let urlinput = document.querySelector('#urlinput');
 let select = document.querySelector('#select');
 let pastebtn = document.querySelector('#pastebtn');
 let main = document.querySelector('main');
-let geturlbtn = document.querySelector('#geturlbtn');
+//let geturlbtn = document.querySelector('#geturlbtn');
 
 let locationURL = window.location.search.replace('?','');
 
@@ -19,7 +19,7 @@ function parse(text) {
 					main.innerHTML = marked(result);
 					break;
 			}
-	});
+		});
 }
 
 // Button
@@ -27,9 +27,10 @@ parsebtn.addEventListener('click', () => {
 	parse(urlinput.value);
 });
 
+/*
 // From URL
 console.log(locationURL);
-if (locationURL) {
+if (locationURL != '') {
 	urlinput.value = locationURL;
 	parse(locationURL);
 }
@@ -37,6 +38,6 @@ if (locationURL) {
 // Get URL
 geturlbtn.addEventListener('click', () => {
 	prompt('Copy this URL', 'https://thundiverter.github.io/marked?' + urlinput.value);
-});
+});*/
 
 // https://github.com/Thundiverter/marked
